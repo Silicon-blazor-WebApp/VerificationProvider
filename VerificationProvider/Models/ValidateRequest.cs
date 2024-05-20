ddb05884ee59e3a251a5b9d55933052bb126805b
hugospangberg <hugospangberg@HUGOSPNGBER517B>
2024-05-20 20:09:09 +0200

    validateVerificationCode completed and tested

---
+++ VerificationProvider/Models/ValidateRequest.cs
@@ -0,0 +1,9 @@
+﻿using System.Drawing;
+
+namespace VerificationProvider.Models;
+
+public class ValidateRequest
+{
+    public string Email { get; set; } = null!;
+    public string Code { get; set; } = null!;
+}
